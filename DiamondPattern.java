033c00296d2f7c249b1b3de5124be7f37d1b2478
Javed Kumail <107232454+javedkumail@users.noreply.github.com>
2023-04-09 03:40:08 +0530

    modiefied diamond pattern bht dimag kharb hua sala

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,60 +1,26 @@
 import java.util.Scanner;
+
 public class DiamondPattern {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int i, j, k;
-        // int rows = sc.nextInt();
-        // i = 1 ;
-        // while(i <= rows )
-        // {
-        //     j = 1 ;
-        //     while( j <= rows - i  )
-        //     {
-        //         System.out.print(" ");
-        //         j++;
-        //     }
-        //     k = 1 ;
-        //     while ( k <= i * 2 - 1 )
-        //     {
-        //         System.out.print("*");
-        //         k++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        // }
-        // i = rows - 1 ;
-        // while( i > 0 )
-        // {
-        //     j = 1 ;
-        //     while ( j <= rows - i )
-        //     {
-        //         System.out.print(" ");
-        //         j++;
-        //     }
-        //     k = 1 ;
-        //     while( k <= i * 2 - 1 )
-        //     {
-        //         System.out.print("*");
-        //         k++;
-        //     }
-        //     System.out.println();
-        //     i--;
-        // }
+        
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
 
         int i = 1;
-        while (i <= n) {
+        int n1 = (n + 1) / 2;
+        int n2 = n / 2;
+        while (i <= n1) {
             // Print spaces before the asterisks
-            int j = 1;
-            while (j <= n - i) {
+            int space = 1;
+            while (space <= n1 - i) {
                 System.out.print(" ");
-                j++;
+                space++;
             }
 
             // Print asterisks
-            j = 1;
-            while (j <= 2 * i - 1) {
+            int j = 1;
+            while (j <= (2 * i) - 1) {
                 System.out.print("*");
                 j++;
             }
@@ -63,22 +29,19 @@ public class DiamondPattern {
             i++;
         }
 
-        i = n - 1;
+        i = n2;
         while (i >= 1) {
-            // Print spaces before the asterisks
-            int j = 1;
-            while (j <= n - i) {
+            int space = 1;
+            while (space <= (n2 - i + 1)) {
                 System.out.print(" ");
-                j++;
+                space++;
             }
 
-            // Print asterisks
-            j = 1;
-            while (j <= 2 * i - 1) {
+            int j = 1;
+            while (j <= (2 * i - 1)) {
                 System.out.print("*");
                 j++;
             }
-
             System.out.println();
             i--;
         }
